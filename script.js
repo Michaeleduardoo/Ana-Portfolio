27df30eec80e84dc1d45f0b1d0c4f3519f59b537
Michaeleduardoo <michaeledu@outlook.com>
2023-03-25 15:54:16 -0300

    Portfolio ANA

---
+++ script.js
@@ -0,0 +1,29 @@
+const iconMenu = document.querySelector(".bxIcon")
+const uL = document.querySelector(".ul-names")
+const one_li = document.querySelector(".a-one")
+const a_conhecimento = document.querySelector(".a-conhecimento")
+const sobremim = document.querySelector(".a-sobremim")
+const fale = document.querySelector(".a-fale")
+
+iconMenu.addEventListener("click", () => {
+    uL.classList.toggle("mobile-menu")
+})
+
+one_li.addEventListener("click", () => {
+    uL.classList.toggle("mobile-menu")
+})
+
+a_conhecimento.addEventListener("click", () => {
+    uL.classList.toggle("mobile-menu")
+})
+
+sobremim.addEventListener("click", () => {
+    uL.classList.toggle("mobile-menu")
+})
+
+fale.addEventListener("click", () => {
+    uL.classList.toggle("mobile-menu")
+})
+
+
+//
